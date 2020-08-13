8facc4532f035e03598fd7ef24f47300c502b4c3
Asequr Rahman <asiq13096@gmail.com>
2020-08-13 22:30:35 +0600

    users

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Require file system module
+var fs = require('file-system');
+
+//Include controllers
+
+fs.readdirSync('controllers').forEach(function (file) {
+  if(file.substr(-3) == '.js') {
+    const route = require('./controllers/' + file)
+    route.controller(app)
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +29,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +45,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000, function(){ console.log('Listening on 3000') })
 
 module.exports = app;
